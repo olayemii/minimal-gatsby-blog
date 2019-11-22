72f49d5eb80b88b8da97f3a3a75c70dc93521eab
OLayemii Garuba <olayemi.garuba@algorismng.com>
2019-11-22 19:49:39 +0100

    FIX: Typography issues

--- src/components/common/pagination/Pagination.js
+++ src/components/common/pagination/Pagination.js
@@ -4,9 +4,9 @@ import paginationStyles from "../../../assets/pagination.module.scss"
 const Pagination = props => {
 	return (
 		<div className={paginationStyles.paginationWrapper}>
-			<div className={paginationStyles.paginationItem}>&laquo; Prev</div>
+			<div className={paginationStyles.paginationItem}>🠐 Prev</div>
 			<div className={paginationStyles.pageCount}>1 of 15</div>
-			<div className={paginationStyles.paginationItem}>Next &raquo;</div>
+			<div className={paginationStyles.paginationItem}>Next →</div>
 		</div>
 	)
 }
