d5293080b40ca82a10881f28a41c697610a7f1b7
OLayemii Garuba <olayemi.garuba@algorismng.com>
2020-01-11 17:29:04 +0100

    'fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,14 @@ import React, { useEffect } from "react"
 import { navigate } from "gatsby"
 
 function NotFound() {
-  useEffect(() => {
+  const redirectHome = () => {
     if (window.location.pathname.match(/^\/page\/1\/?$/)) {
-      window.location.href = "/"
-    } else {
-      console.log(window.location.pathname)
+      return true
     }
-  }, [])
-  return <div>Page Not Found</div>
+
+    return false
+  }
+  return redirectHome() ? <Redirect to={"/"} /> :<div>Page Not Found</div>
 }
 
 export default NotFound
