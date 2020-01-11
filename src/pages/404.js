2bd3fac0c56e000b59b4c5acba86a69182fa1a79
OLayemii Garuba <olayemi.garuba@algorismng.com>
2020-01-11 18:05:07 +0100

    fix window

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,13 @@ import React, { useEffect } from "react"
 import { Redirect } from "@reach/router"
 function NotFound() {
   const redirectHome = () => {
+    // work
+    if (window) {
+      if (window.location.pathname.match(/^\/page\/1\/?$/)) {
+        return true
+      }
+    }
+
     return false
   }
   return redirectHome() ? <Redirect to={"/"} /> : <div>Page Not Found</div>
