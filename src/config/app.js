6058b870cfa610336468ffe497808b230022352a
OLayemii Garuba <olayemi.garuba@algorismng.com>
2020-01-12 16:46:55 +0100

    FIX adn additions

---
+++ src/config/app.js
@@ -0,0 +1,11 @@
+const Config = {
+  title: "Severus Snape",
+  titleTemplate: "%s · The Real Hero",
+  description:
+    "Hogwarts Potions master, Head of Slytherin house and former Death Eater.",
+  url: "https://www.doe.com", // No trailing slash allowed!
+  image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
+  twitterUsername: "@occlumency",
+}
+
+module.exports = Config
